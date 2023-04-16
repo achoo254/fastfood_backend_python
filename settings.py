f48c85fcf88bf2f62e0f23f433b9bb52bd85d0eb
dattqh@inet.vn <D@tsh150>
2023-04-16 22:35:30 +0700

    update

--- settings.py
+++ settings.py
@@ -1,3 +1,17 @@
-HOST = 'localhost'
+import os
+from dotenv import load_dotenv
+
 PORT = 8088
 LOG_LEVEL = 'INFO'
+
+# Need to check and return boolean, so check for String "True"
+APP_ENVIRONMENT = os.getenv("DEBUG") == "True"
+
+if APP_ENVIRONMENT:
+    load_dotenv(".env.development")
+else:
+    load_dotenv(".env.production")
+
+HOST = os.environ.get("HOST")
+
+print(HOST)
